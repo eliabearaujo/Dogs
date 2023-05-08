eb1f41013dcd45efd7ffc2d12208b6aaef764045
eliabearaujo <eliabe_trajano@hotmail.com>
2023-05-08 00:25:09 -0300

    Comments iniciado.

--- src/Components/Feed/FeedModal.js
+++ src/Components/Feed/FeedModal.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import styles from './FeedModal.module.css';
 import useFetch from '../../Hooks/useFetch';
-import { PHOTOS_GET, PHOTO_GET } from '../../api';
+import { PHOTO_GET } from '../../api';
+import Erro from '../InterfaceElements/Erro';
+import Loading from '../InterfaceElements/Loading';
+import PhotoContent from '../Photo/PhotoContent';
 
 const FeedModal = ({ photo }) => {
   const { data, error, loading, request } = useFetch();
@@ -9,9 +12,15 @@ const FeedModal = ({ photo }) => {
   React.useEffect(() => {
     const { url, options } = PHOTO_GET(photo.id);
     request(url, options);
-  });
+  }, [photo, request]);
 
-  return <div className={styles.modal}>FeedModal</div>;
+  return (
+    <div className={styles.modal}>
+      {error && <Erro error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
+    </div>
+  );
 };
 
 export default FeedModal;
