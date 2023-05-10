b8a0ef194a185adc9c1fec527ea56fbc28c59eb7
eliabearaujo <eliabe_trajano@hotmail.com>
2023-05-10 00:53:37 -0300

    Image placeholder criado.

--- src/Components/Feed/FeedPhotosItem.js
+++ src/Components/Feed/FeedPhotosItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './FeedPhotosItem.module.css';
+import Img from '../InterfaceElements/Img';
 
 const FeedPhotosItem = ({ photo, setModalPhoto }) => {
   function handleClick() {
@@ -8,7 +9,7 @@ const FeedPhotosItem = ({ photo, setModalPhoto }) => {
 
   return (
     <li className={styles.photo} onClick={handleClick}>
-      <img src={photo.src} alt={photo.title} />
+      <Img src={photo.src} alt={photo.title} />
       <span className={styles.visualizacao}>{photo.acessos}</span>
     </li>
   );
