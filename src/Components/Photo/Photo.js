d4e4dbd1f23546c9c81bfd64ee27858b06ece6f7
eliabearaujo <eliabe_trajano@hotmail.com>
2023-05-13 03:25:49 -0300

    Photo page finalizado.

--- src/Components/Photo/Photo.js
+++ src/Components/Photo/Photo.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import useFetch from '../../Hooks/useFetch';
+import { PHOTO_GET } from '../../api';
+import Erro from '../InterfaceElements/Erro';
+import Loading from '../InterfaceElements/Loading';
+import PhotoContent from './PhotoContent';
 
 const Photo = () => {
-  return <div>Photo</div>;
+  const { id } = useParams();
+  const { data, loading, error, request } = useFetch();
+
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(id);
+    request(url, options);
+  }, [id, request]);
+  if (error) return <Erro error={error} />;
+  if (loading) return <Loading />;
+  if (data)
+    return (
+      <section className="container mainContainer">
+        <PhotoContent single={true} data={data} />
+      </section>
+    );
+  else {
+    return null;
+  }
 };
 
 export default Photo;
