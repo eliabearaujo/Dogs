ea58bb9a7134f73e16acd2708bdaa23c717162e0
eliabearaujo <eliabe_trajano@hotmail.com>
2023-05-14 23:26:22 -0300

    Footer corrigido.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,7 +6,11 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <DogsFooter />
-      <p>Dogs. Alguns direitos reservados.</p>
+      <p>
+        Desenvolvido para A3 de Usabilidade, desenvolvimento web, mobile e
+        jogos.
+      </p>
+      <p>Eliabe Trajano de Araújo - 125111372575</p>
     </footer>
   );
 };
